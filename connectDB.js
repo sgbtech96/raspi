cd2375f0fca5e4b5509f9355780dc4e8aaaed207
unknown <sgbtech96@gmail.com>
2020-03-08 00:33:59 +0530

    updated

--- connectDB.js
+++ connectDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('./model')
-mongoose.connect('mongodb://127.0.0.1:mongodb+srv://sgbtech96:<nptel@92>@cluster0-hluvl.mongodb.net/test?retryWrites=true&w=majority/apurv', {
+mongoose.connect('mongodb+srv://sgbtech96:<nptel@92>@cluster0-hluvl.mongodb.net/test?retryWrites=true&w=majority/apurv', {
 	useNewUrlParser: true,
 	useCreateIndex: true
 })
